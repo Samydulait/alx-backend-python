b9b4fa7872cab7539ba877c8c8c49b1348862924
Samydulait <Samydulait>
2023-07-07 02:30:36 +0000

    Update to solution

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""Write a type-annotated function add that takes a float a and a
-float b as arguments and returns their sum as a float.
+"""Write a type-annotated function concat that takes a string str1
+and a string str2 as arguments and returns a concatenated string
 """
 
 
-def add(a: float, b: float) -> float:
-    """Add two floats"""
-    return (a + b)
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings"""
+    return str1 + str2
